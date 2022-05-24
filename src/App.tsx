2b6c4e1532eea9df705636cd3b61a5713dc9c397
Jesse Navarro <jnavarro30@live.com>
2022-05-24 02:01:30 -0700

    setup react mantine and  tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="border-2 m-4">
+      Hello
+    </div>
+  );
+}
+
+export default App;
